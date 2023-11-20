9336b0a0489cfeaa4313a70d7b85bef148e6ed51
praveen325940 <bpkcloudengineer325940@gmail.com>
2023-11-20 15:58:10 +0530

    updated ami and region

--- AWS_ec2.tf
+++ AWS_ec2.tf
@@ -1,11 +1,11 @@
 # main.tf
 
 provider "aws" {
-  region = "us-east-1" # Set your desired AWS region
+  region = "ap-south-1" # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-xxxxxxxxxxxxxxxxx" # Replace with your desired AMI ID
+resource "aws_instance" "instance1" {
+  ami           = "ami-02a2af70a66af6dfb" # Replace with your desired AMI ID
   instance_type = "t2.micro"              # Replace with your desired instance type
 
   tags = {
